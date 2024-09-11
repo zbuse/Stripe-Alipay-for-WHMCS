c923f5ccec0fa2d0831e62fd7484434bc6fdf4aa
zbuse <5518208+zbuse@users.noreply.github.com>
2024-09-11 15:20:16 +0800

    Update webhooks.php

--- modules/gateways/stripealipay/webhooks.php
+++ modules/gateways/stripealipay/webhooks.php
@@ -6,12 +6,16 @@ require_once __DIR__ . '/../../../init.php';
 require_once __DIR__ . '/../../../includes/gatewayfunctions.php';
 require_once __DIR__ . '/../../../includes/invoicefunctions.php';
 
-$gatewayName = 'stripealipay';
-$Params = getGatewayVariables($gatewayName);
+
+
+$gatewayParams = getGatewayVariables('stripealipay');
+$gatewayName = $gatewayParams['name'];
+
+
 if (!defined("WHMCS")) {
     die("This file cannot be accessed directly");
 }
-if (!$Params['type']) {
+if (!$gatewayParams'type']) {
     die("Module Not Activated");
 }
 if (!isset($_SERVER['HTTP_STRIPE_SIGNATURE'])) {
@@ -24,46 +28,48 @@ $event = null;
 
 try {
     $event = Webhook::constructEvent(
-        $payload, $sig_header, $Params['StripeWebhookKey']
+        $payload, $sig_header, $gatewayParams'StripeWebhookKey']
     );
 } catch(\UnexpectedValueException $e) {
-    logTransaction($Params['paymentmethod'], $e, $gatewayName.': Invalid payload');
+    logTransaction($gatewayName, $e, $gatewayName.': Invalid payload');
     http_response_code(400);
     exit();
 } catch(Stripe\Exception\SignatureVerificationException $e) {
-    logTransaction($Params['paymentmethod'], $e, $gatewayName.': Invalid signature');
+    logTransaction($gatewayName, $e, $gatewayName.': Invalid signature');
     http_response_code(400);
     exit();
 }
 
 try {
     if ($event->type == 'payment_intent.succeeded') {
-        $stripe = new Stripe\StripeClient($Params['StripeSkLive']);
+        $stripe = new Stripe\StripeClient($gatewayParams'StripeSkLive']);
         $paymentId = $event->data->object->id;
 
         $paymentIntent = $stripe->paymentIntents->retrieve($paymentId,[]);
 
         if ($paymentIntent->status == 'succeeded') {
-            $invoiceId = checkCbInvoiceID($paymentIntent['metadata']['invoice_id'], $Params['paymentmethod']);
+            $invoiceId = checkCbInvoiceID($paymentIntent['metadata']['invoice_id'], $gatewayName);
 	    checkCbTransID($paymentId);
 		
         //Get Transactions fee
         $charge = $stripe->charges->retrieve($paymentIntent->latest_charge, []);
         $balanceTransaction = $stripe->balanceTransactions->retrieve($charge->balance_transaction, []);
         $fee = $balanceTransaction->fee / 100.00;
-if ( strtoupper($params['currency']) != strtoupper($balanceTransaction->currency )) {
-        $feeexchange = stripealipay_exchange($params['currency'], strtoupper($balanceTransaction->currency ));
+	$invoice = Capsule::table('tblinvoices')->where('id', $invoiceId)->first();  //获取账单信息和用户 id
+	$currency = getCurrency( $invoice->userid ); //获取用户使用货币信息
+		
+if ( strtoupper($currency['code'])  != strtoupper($balanceTransaction->currency )) {
+        $feeexchange = stripewechatpay_exchange($currency['code'], strtoupper($balanceTransaction->currency ));
         $fee = floor($balanceTransaction->fee * $feeexchange / 100.00);
 }
-		
-            logTransaction($Params['paymentmethod'], $paymentIntent, $gatewayName.': Callback successful');
-             addInvoicePayment($invoiceId, $paymentId,$paymentIntent['metadata']['original_amount'],$fee,$params['paymentmethod']);
+            logTransaction($gatewayName, $paymentIntent, $gatewayName.': Callback successful');
+             addInvoicePayment($invoiceId, $paymentId,$invoice->total,$fee,$gatewayName);
 		}
             echo json_encode(['status' => $paymentIntent->status ]);
     }
     
 } catch (Exception $e) {
-    logTransaction($Params['paymentmethod'], $e, 'error-callback');
+    logTransaction($gatewayName, $e, 'error-callback');
     http_response_code(400);
     echo $e;
 }
